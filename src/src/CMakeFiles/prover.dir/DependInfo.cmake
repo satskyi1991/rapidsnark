4cd2b2727692c288eced44bfe058c61280d76a2e
illia <i.satskyi@gmail.com>
2022-02-12 14:09:07 +0200

    project with built cmake

---
+++ src/src/CMakeFiles/prover.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/build/fq.cpp" "src/CMakeFiles/prover.dir/__/build/fq.cpp.o" "gcc" "src/CMakeFiles/prover.dir/__/build/fq.cpp.o.d"
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/build/fr.cpp" "src/CMakeFiles/prover.dir/__/build/fr.cpp.o" "gcc" "src/CMakeFiles/prover.dir/__/build/fr.cpp.o.d"
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/depends/ffiasm/c/alt_bn128.cpp" "src/CMakeFiles/prover.dir/__/depends/ffiasm/c/alt_bn128.cpp.o" "gcc" "src/CMakeFiles/prover.dir/__/depends/ffiasm/c/alt_bn128.cpp.o.d"
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/depends/ffiasm/c/misc.cpp" "src/CMakeFiles/prover.dir/__/depends/ffiasm/c/misc.cpp.o" "gcc" "src/CMakeFiles/prover.dir/__/depends/ffiasm/c/misc.cpp.o.d"
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/depends/ffiasm/c/naf.cpp" "src/CMakeFiles/prover.dir/__/depends/ffiasm/c/naf.cpp.o" "gcc" "src/CMakeFiles/prover.dir/__/depends/ffiasm/c/naf.cpp.o.d"
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/depends/ffiasm/c/splitparstr.cpp" "src/CMakeFiles/prover.dir/__/depends/ffiasm/c/splitparstr.cpp.o" "gcc" "src/CMakeFiles/prover.dir/__/depends/ffiasm/c/splitparstr.cpp.o.d"
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/src/binfile_utils.cpp" "src/CMakeFiles/prover.dir/binfile_utils.cpp.o" "gcc" "src/CMakeFiles/prover.dir/binfile_utils.cpp.o.d"
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/src/logger.cpp" "src/CMakeFiles/prover.dir/logger.cpp.o" "gcc" "src/CMakeFiles/prover.dir/logger.cpp.o.d"
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/src/main_prover.cpp" "src/CMakeFiles/prover.dir/main_prover.cpp.o" "gcc" "src/CMakeFiles/prover.dir/main_prover.cpp.o.d"
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/src/wtns_utils.cpp" "src/CMakeFiles/prover.dir/wtns_utils.cpp.o" "gcc" "src/CMakeFiles/prover.dir/wtns_utils.cpp.o.d"
+  "/home/illia/Documents/Armlib/rapidsnark/rapidsnark/src/zkey_utils.cpp" "src/CMakeFiles/prover.dir/zkey_utils.cpp.o" "gcc" "src/CMakeFiles/prover.dir/zkey_utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/src/CMakeFiles/prover.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prover.dir/__/build/fq.cpp.o"
+  "CMakeFiles/prover.dir/__/build/fq.cpp.o.d"
+  "CMakeFiles/prover.dir/__/build/fr.cpp.o"
+  "CMakeFiles/prover.dir/__/build/fr.cpp.o.d"
+  "CMakeFiles/prover.dir/__/depends/ffiasm/c/alt_bn128.cpp.o"
+  "CMakeFiles/prover.dir/__/depends/ffiasm/c/alt_bn128.cpp.o.d"
+  "CMakeFiles/prover.dir/__/depends/ffiasm/c/misc.cpp.o"
+  "CMakeFiles/prover.dir/__/depends/ffiasm/c/misc.cpp.o.d"
+  "CMakeFiles/prover.dir/__/depends/ffiasm/c/naf.cpp.o"
+  "CMakeFiles/prover.dir/__/depends/ffiasm/c/naf.cpp.o.d"
+  "CMakeFiles/prover.dir/__/depends/ffiasm/c/splitparstr.cpp.o"
+  "CMakeFiles/prover.dir/__/depends/ffiasm/c/splitparstr.cpp.o.d"
+  "CMakeFiles/prover.dir/binfile_utils.cpp.o"
+  "CMakeFiles/prover.dir/binfile_utils.cpp.o.d"
+  "CMakeFiles/prover.dir/logger.cpp.o"
+  "CMakeFiles/prover.dir/logger.cpp.o.d"
+  "CMakeFiles/prover.dir/main_prover.cpp.o"
+  "CMakeFiles/prover.dir/main_prover.cpp.o.d"
+  "CMakeFiles/prover.dir/wtns_utils.cpp.o"
+  "CMakeFiles/prover.dir/wtns_utils.cpp.o.d"
+  "CMakeFiles/prover.dir/zkey_utils.cpp.o"
+  "CMakeFiles/prover.dir/zkey_utils.cpp.o.d"
+  "prover"
+  "prover.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prover.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
