31c204a5b07692a552b247ca041cede45da39348
illia <i.satskyi@gmail.com>
2022-02-19 14:55:36 +0200

    Added cmake files

---
+++ build/CMakeFiles/test_fr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_fr.dir/fr.cpp.o"
+  "CMakeFiles/test_fr.dir/main.cpp.o"
+  "test_fr"
+  "test_fr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_fr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ depends/ffiasm/test/tester/tester.pro
@@ -0,0 +1,4 @@
+CONFIG -= qt
+CONFIG += console c++11
+SOURCES = tester.cpp
+LIBS += -lgmp
