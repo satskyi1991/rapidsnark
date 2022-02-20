2b8f49b55550186e1a143a68da92d384d394a138
illia <i.satskyi@gmail.com>
2022-02-20 16:04:41 +0200

    Fr_rawToMontgomery passed a test

--- build/main.cpp
+++ build/main.cpp
@@ -203,9 +203,8 @@ int main()
     //Fr_Rw_neg_test();
     //Fr_Rw_mul_test();
     //Fr_Rw_square_test();
-    Fr_Rw_mul1_test();
-
-    //Fr_Rw_ToMontgomery_test();
+    //Fr_Rw_mul1_test();
+    Fr_Rw_ToMontgomery_test();
 
 //    std::cout << typeid(uint64_t).name() << '\n';
 //    std::cout << typeid(FrRawElement).name() << '\n';
