f36056b52ce532550c829b8be1a09a57b510ae7f
illia <i.satskyi@gmail.com>
2022-03-15 11:54:35 +0200

    Code clean up

--- build/fr.cpp
+++ build/fr.cpp
@@ -1292,35 +1292,6 @@ void Fr_mul(PFrElement r, PFrElement a, PFrElement b)
         //mul_s1s2:
          std::cout << "mul_s1s2: " <<  '\n';
          mul_s1s2(r, a, b);
-
-//         //mul_s1s2(PFrElement r, PFrElement a, PFrElement b)
-//         {
-//             int64_t temp = (int64_t)a->shortVal * (int64_t)b->shortVal;
-
-//             //mpz_import(rax, Fr_N64, -1, 8, -1, 0, (const void *)temp);
-////             mpz_import(rax, Fr_N64, -1, 8, -1, 0, (const void *)temp);
-////             rax->_mp_d[0] = temp;
-//             r->longVal[0] = temp;
-//             mpz_import(rax, Fr_N64, -1, 8, -1, 0, (const void *)r);
-
-
-//             if (!mpz_fits_sint_p(rax)) //if (mul_manageOverflow)
-//             {
-//                 r->longVal[0] = temp; // с расширением знака до 256 бит
-
-//                    if (temp < 0)
-//                    {
-//                        r->longVal[0] += Fr_q.longVal[0];    //сложение 256 битных чисел
-//                        r->longVal[1] += Fr_q.longVal[1];
-//                        r->longVal[2] += Fr_q.longVal[2];
-//                        r->longVal[3] += Fr_q.longVal[3];
-//                    }
-//             }
-//             else
-//             {
-
-//             }
-//         }
     }
     mpz_clear(ma);
     mpz_clear(mb);
@@ -1344,12 +1315,12 @@ void mul_s1s2(PFrElement r, PFrElement a, PFrElement b)
     // mul_manageOverflow
     if (!mpz_fits_sint_p(rax))
     {
-        std::cout << "mul_s1s2: rawCopyS2L " <<  '\n';
+        //std::cout << "mul_s1s2: rawCopyS2L " <<  '\n';
         rawCopyS2L(r, temp);
     }
     else
     {
-        std::cout << "mul_s1s2: not in rawCopyS2L " <<  '\n';
+        //std::cout << "mul_s1s2: not in rawCopyS2L " <<  '\n';
         r->type = Fr_SHORT;
         r->shortVal = temp;
     }
@@ -1363,7 +1334,7 @@ void rawCopyS2L(PFrElement r, int64_t temp)
 
     if (temp < 0)
     {
-       std::cout << "mul_s1s2: rawCopyS2L 2" <<  '\n';
+       //std::cout << "mul_s1s2: rawCopyS2L 2" <<  '\n';
        r->longVal[0] += Fr_q.longVal[0];    //сложение 256 битных чисел
        r->longVal[1] += Fr_q.longVal[1];
        r->longVal[2] += Fr_q.longVal[2];
